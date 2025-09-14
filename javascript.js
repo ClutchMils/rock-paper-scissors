2093bc2f2047ac3b1448902e7ec667c021bcbb78
“ClutchMils” <220502194+ClutchMils@users.noreply.github.com>
2025-09-13 21:29:53 -0500

    build: function that prompts user for input and return it

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 // console.log("hello");
 
 function getComputerChoice(){
-    // let choice = prompt("Enter Rock, Paper, or Scissors");
+    
     const min = 1;
     const max = 3;
     const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,3 +10,11 @@ function getComputerChoice(){
 }
 
 //  console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt("Enter rock, paper, or scissors","c");
+
+    return choice;
+}
+
+// console.log(getHumanChoice());
